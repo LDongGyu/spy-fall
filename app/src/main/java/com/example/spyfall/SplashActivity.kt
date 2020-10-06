c5409bb63ddc7d375927399dd1b46565277465c1
LDongGyu <ehdrbchd@naver.com>
2020-10-06 21:37:00 +0900

    feat: Splash에서 TutorialActivity 분기 연동

--- app/src/main/java/com/example/spyfall/SplashActivity.kt
+++ app/src/main/java/com/example/spyfall/SplashActivity.kt
@@ -1,6 +1,8 @@
 package com.example.spyfall
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -11,10 +13,18 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        var sharedPreferences = getSharedPreferences("isFirst", MODE_PRIVATE)
+
         Handler().postDelayed({
+            if(sharedPreferences.getBoolean("first",true)){
+                sharedPreferences.edit().putBoolean("first",false).commit()
+                startActivity(Intent(this, TutorialActivity::class.java))
+                finish()
+            }
+            else{
                 startActivity(Intent(this, MainActivity::class.java))
                 finish()
-
+            }
         },2000)
     }
 }
