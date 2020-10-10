49cc27eeecc6193ec0e3846ba3545b358933dc42
LDongGyu <ehdrbchd@naver.com>
2020-10-10 23:14:26 +0900

    feat: GameSettingActivity 추가

---
+++ app/src/main/java/com/example/spyfall/common/GameSettingActivity.kt
@@ -0,0 +1,13 @@
+package com.example.spyfall.common
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.spyfall.R
+
+class GameSettingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_setting)
+    }
+}
