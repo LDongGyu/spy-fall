5dae77b379c961d8e8f3157d72ff869ecfd6bfeb
LDongGyu <ehdrbchd@naver.com>
2020-10-10 23:14:53 +0900

    feat: GameSettingActivity 연동

--- app/src/main/java/com/example/spyfall/common/MainActivity.kt
+++ app/src/main/java/com/example/spyfall/common/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.example.spyfall.common
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.spyfall.R
+import com.example.spyfall.common.Tutorial.TutorialActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        game_btn.setOnClickListener {
+            startActivity(Intent(this,GameSettingActivity::class.java))
+            finish()
+        }
+
+        tutorial_btn.setOnClickListener {
+            startActivity(Intent(this, TutorialActivity::class.java))
+            finish()
+        }
     }
 }
