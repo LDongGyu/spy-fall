83e8da3aba5a21e996cb68b1fe6566604447c18c
LDongGyu <ehdrbchd@naver.com>
2020-10-11 22:34:27 +0900

    refactor: mainActivity ClickListener 분리

--- app/src/main/java/com/example/spyfall/common/MainActivity.kt
+++ app/src/main/java/com/example/spyfall/common/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.spyfall.common
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.spyfall.R
 import com.example.spyfall.common.Tutorial.TutorialActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,14 +14,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        game_btn.setOnClickListener {
-            startActivity(Intent(this,GameSettingActivity::class.java))
-            finish()
-        }
+        game_btn.setOnClickListener(gameBtnClickListener)
+        tutorial_btn.setOnClickListener(tutorialBtnClickListener)
+    }
 
-        tutorial_btn.setOnClickListener {
-            startActivity(Intent(this, TutorialActivity::class.java))
-            finish()
-        }
+    private val gameBtnClickListener: View.OnClickListener = View.OnClickListener {
+        startActivity(Intent(this,GameSettingActivity::class.java))
+        finish()
+    }
+
+    private val tutorialBtnClickListener: View.OnClickListener = View.OnClickListener {
+        startActivity(Intent(this, TutorialActivity::class.java))
+        finish()
     }
 }
