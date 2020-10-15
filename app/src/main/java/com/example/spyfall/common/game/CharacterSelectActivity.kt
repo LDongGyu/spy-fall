99283626c8ca928172b0e005bd992ab6a564c7ae
LDongGyu <ehdrbchd@naver.com>
2020-10-15 21:51:58 +0900

    feat: CharacterSelectActivity 추가

---
+++ app/src/main/java/com/example/spyfall/common/game/CharacterSelectActivity.kt
@@ -0,0 +1,13 @@
+package com.example.spyfall.common.game
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.spyfall.R
+
+class CharacterSelectActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_character_select)
+    }
+}
