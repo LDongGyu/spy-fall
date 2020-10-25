a2d52c59d3e130558c2b8025584665d8b152e498
LDongGyu <ehdrbchd@naver.com>
2020-10-25 15:19:54 +0900

    feat: Card Animation Demo 추가

--- app/src/main/java/com/example/spyfall/common/game/CharacterSelectActivity.kt
+++ app/src/main/java/com/example/spyfall/common/game/CharacterSelectActivity.kt
@@ -1,14 +1,21 @@
 package com.example.spyfall.common.game
 
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.room.Room
 import com.example.spyfall.R
 import com.example.spyfall.database.GameInfoDataBase
+import kotlinx.android.synthetic.main.activity_character_select.*
 import kotlin.random.Random
 
 class CharacterSelectActivity : AppCompatActivity() {
+
+    lateinit var front_anim: AnimatorSet
+    lateinit var back_anim: AnimatorSet
+    var isFront = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,5 +37,29 @@ class CharacterSelectActivity : AppCompatActivity() {
         }
         val dbThread = Thread(dbRead)
         dbThread.start()
+
+        val scale = applicationContext.resources.displayMetrics.density
+        card_front.cameraDistance = 8000 * scale
+        card_back.cameraDistance = 8000 * scale
+
+        front_anim = AnimatorInflater.loadAnimator(applicationContext,R.animator.front_animator) as AnimatorSet
+        back_anim = AnimatorInflater.loadAnimator(applicationContext,R.animator.back_animator) as AnimatorSet
+
+        flip_btn.setOnClickListener {
+            if(isFront){
+                front_anim.setTarget(card_front)
+                back_anim.setTarget(card_back)
+                front_anim.start()
+                back_anim.start()
+                isFront = false
+            }
+            else{
+                front_anim.setTarget(card_back)
+                back_anim.setTarget(card_front)
+                back_anim.start()
+                front_anim.start()
+                isFront = true
+            }
+        }
     }
 }
