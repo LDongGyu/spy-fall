7ff82ed4131c5adf0bcf10e1b4452281e9b1568e
LDongGyu <ehdrbchd@naver.com>
2020-11-04 22:57:02 +0900

    refactor: timer fragment로 변경

--- app/src/main/java/com/example/spyfall/common/game/GameTimerActivity.kt
+++ app/src/main/java/com/example/spyfall/common/game/GameTimerActivity.kt
@@ -3,12 +3,12 @@ package com.example.spyfall.common.game
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.CountDownTimer
-import android.util.Log
 import android.view.View
 import com.example.spyfall.R
 import com.example.spyfall.common.dashboard.MainActivity
+import com.example.spyfall.common.game.fragment.TimerFragment
 import kotlinx.android.synthetic.main.activity_game_timer.*
+import kotlinx.android.synthetic.main.fragment_result.*
 
 class GameTimerActivity : AppCompatActivity() {
 
@@ -19,47 +19,29 @@ class GameTimerActivity : AppCompatActivity() {
         setContentView(R.layout.activity_game_timer)
 
         var totalTime = intent.getIntExtra("time",480).toLong()
-        Log.d("timer",totalTime.toString())
+        var fragmentManager = supportFragmentManager
+        var fragmentTransaction = fragmentManager.beginTransaction()
+        var fragment = TimerFragment()
+        var bundle = Bundle()
+        bundle.putLong("time",totalTime)
+        fragment.arguments = bundle
 
-        var downTimer = object : CountDownTimer(totalTime * 1000,1000){
-            override fun onFinish() {
-                timer.text = "Finish!"
-                openBtn.visibility = View.VISIBLE
-            }
-
-            override fun onTick(p0: Long) {
-                Log.d("timer",(p0/1000).toString())
-                var time = p0/1000
-                var min = (time - time%60)/60
-                var second = time%60
-                var minText = ""
-                var secondText = ""
-
-                if(min < 10)  minText = String.format("0%d",min)
-                else minText = min.toString()
-
-                if(second < 10) secondText = String.format("0%d",second)
-                else secondText = second.toString()
-
-                timer.text = String.format("%s:%s",minText,secondText)
-            }
-
-        }
-
-        downTimer.start()
+        fragmentTransaction.add(R.id.info,fragment)
+        fragmentTransaction.commit()
 
         openBtn.setOnClickListener(openBtnClickListener)
     }
 
     val openBtnClickListener: View.OnClickListener = View.OnClickListener {
         if(version == 0) {
-            timer.text = "스파이는 ${intent.getIntExtra("spy", 0)}번이었습니다."
+            answer.text = "스파이는 ${intent.getIntExtra("spy", 0)}번이었습니다."
             openBtn.text = "메인으로"
             version++
         }
         else {
-            startActivity(Intent(this,MainActivity::class.java))
+            startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
     }
+
 }
