791953ff1c2d449f3aad3e1af1a457bd20e216a2
LDongGyu <ehdrbchd@naver.com>
2020-11-06 22:42:35 +0900

    feat: 결과 확인 fragment에 값 넘겨주기

--- app/src/main/java/com/example/spyfall/common/game/GameTimerActivity.kt
+++ app/src/main/java/com/example/spyfall/common/game/GameTimerActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import com.example.spyfall.R
 import com.example.spyfall.common.dashboard.MainActivity
+import com.example.spyfall.common.game.fragment.ResultFragment
 import com.example.spyfall.common.game.fragment.TimerFragment
 import kotlinx.android.synthetic.main.activity_game_timer.*
 import kotlinx.android.synthetic.main.fragment_result.*
@@ -13,19 +14,18 @@ import kotlinx.android.synthetic.main.fragment_result.*
 class GameTimerActivity : AppCompatActivity() {
 
     private var version = 0
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_timer)
 
         var totalTime = intent.getIntExtra("time",480).toLong()
-        var fragmentManager = supportFragmentManager
-        var fragmentTransaction = fragmentManager.beginTransaction()
+
         var fragment = TimerFragment()
         var bundle = Bundle()
         bundle.putLong("time",totalTime)
         fragment.arguments = bundle
 
+        var fragmentTransaction = supportFragmentManager.beginTransaction()
         fragmentTransaction.add(R.id.info,fragment)
         fragmentTransaction.commit()
 
@@ -34,7 +34,17 @@ class GameTimerActivity : AppCompatActivity() {
 
     val openBtnClickListener: View.OnClickListener = View.OnClickListener {
         if(version == 0) {
-            answer.text = "스파이는 ${intent.getIntExtra("spy", 0)}번이었습니다."
+            var spy = intent.getIntExtra("spy",1)
+            var place =  intent.getStringExtra("place")
+            var bundle = Bundle()
+            bundle.putInt("spy",spy)
+            bundle.putString("place",place)
+
+            var fragment = ResultFragment()
+            fragment.arguments = bundle
+            var fragmentTransaction = supportFragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.info,fragment)
+            fragmentTransaction.commit()
             openBtn.text = "메인으로"
             version++
         }
--- app/src/main/java/com/example/spyfall/common/game/fragment/TimerFragment.kt
+++ app/src/main/java/com/example/spyfall/common/game/fragment/TimerFragment.kt
@@ -12,19 +12,6 @@ import android.view.ViewGroup
 import com.example.spyfall.R
 import kotlinx.android.synthetic.main.fragment_timer.view.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [TimerFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [TimerFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TimerFragment() : Fragment() {
 
     private var total: Long = 0
