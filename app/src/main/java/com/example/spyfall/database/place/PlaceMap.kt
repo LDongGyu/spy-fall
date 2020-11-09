9729ccc7217ddee9a49fba7a11e7a0d93fd38b9d
LDongGyu <ehdrbchd@naver.com>
2020-11-09 12:51:10 +0900

    feat: 장소 변경에 따른 Map 수정

--- app/src/main/java/com/example/spyfall/database/place/PlaceMap.kt
+++ app/src/main/java/com/example/spyfall/database/place/PlaceMap.kt
@@ -3,6 +3,8 @@ package com.example.spyfall.database.place
 import com.example.spyfall.R
 
 object PlaceMap {
-    val placeMap = mapOf("아이즈원" to R.drawable.izone, "학교" to R.drawable.school, "영화관" to R.drawable.movie, "코다차야" to R.drawable.pub)
+    val placeMap = mapOf(
+        "대학교" to R.drawable.school, "경찰서" to R.drawable.police, "놀이공원" to R.drawable.amusement, "은행" to R.drawable.bank,
+        "병원" to R.drawable.hospital, "레스토랑" to R.drawable.restaurant, "술집" to R.drawable.pub, "공항" to R.drawable.airport,
+        "군대" to R.drawable.military, "콘서트장" to R.drawable.concert)
 }
-
